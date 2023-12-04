60e26048c053b329c8a6dde1c6548d320a12af89
alaa <you@example.com>
2023-12-05 01:23:03 +0200

    uplod

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// // Fetch the PDE file content from the server
+// fetch("../files/a.pdf")
+//   .then(response => response.text())
+//   .then(data => {
+//     // Display the PDE file content in the HTML element
+//     document.getElementById("pdeContent").textContent = data;
+//   })
+//   .catch(error => {
+//     console.log("Error fetching PDE file:", error);
+//   });
